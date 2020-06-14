5204095ea24a47ec1237b5f914a33e1cde74e5a8
SomberTM <50004094+SomberTM@users.noreply.github.com>
2020-06-13 21:19:21 -0400

    Delete isID.js

--- src/utils/isID.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isID = void 0;
-function isID(id) {
-    return id.match(/([0-9])+/g) ? true : false;
-}
-exports.isID = isID;
